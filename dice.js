7880af5ac91097fade3650d9108119f272873084
lambesoan <146779905+lambesoan@users.noreply.github.com>
2023-12-12 01:28:36 -0800

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,56 @@
+// note: all of the variables are global variables
+var die1, die2, dieSum, balance, banner, outcome, die1Name, die2Name, numRolls; // define variables
+balance = 0;  // initial value
+var die1Image = new Image();
+var die2Image = new Image();
+
+function rollDice() {
+    die1 = Math.floor(Math.random() * 6) + 1;
+    die2 = Math.floor(Math.random() * 6) + 1;
+    dieSum = die1 + die2;
+
+    // set the dice images based on the roll
+    die1Image = document.querySelectorAll("img")[0];
+    die1Name = "images/dice" + die1 + ".png";
+    die1Image.setAttribute("src", die1Name);
+
+    die2Image = document.querySelectorAll("img")[1];
+    die2Name = "images/dice" + die2 + ".png";
+    die2Image.setAttribute("src", die2Name);
+}
+
+function whoWon() {
+    // in h3 report how much money was won or lost and the balance
+    if (dieSum <= 5){
+      outcome = "You lose, please pay me " + 5 + " dollars.";
+      balance -= 5;
+    } else if (dieSum >= 9){
+      outcome = "You win, I pay you " + 5 + " dollars.";
+      balance += 5;
+    } else {
+      outcome = "Its a draw, nobody wins or loses.";
+    }
+
+    // Report the outcome:
+    banner = die1 + " + " + die2 + " is: " + dieSum;
+    document.querySelector("h3").innerHTML = banner + "<br>" + outcome;
+}
+
+function letsPlay(){
+  rollDice();
+  whoWon();
+}
+// die game lab changes/ result
+async function letsPlay(){
+  var input = window.prompt("How many games do you want to play?")
+  // This is what pops up the banner where the user inputs the information
+  if(!!input && !isNaN(input)){
+      for(var i = 0 ; i<input; i++){
+          //this is the forloop of the section
+          rollDice();
+          await whoWon();
+      }
+      alert("You won = " +balance +", come again soon!")
+// final outcome exiting the loop
+    }
+}
